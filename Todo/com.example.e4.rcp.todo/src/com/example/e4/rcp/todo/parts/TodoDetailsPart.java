185da1e4dc8a0b71bcc05b58195b0bc29df42fd0
frank <frankzhang2010@foxmail.com>
2016-04-01 11:08:11 +0800

    add todo

---
+++ Todo/com.example.e4.rcp.todo/src/com/example/e4/rcp/todo/parts/TodoDetailsPart.java
@@ -0,0 +1,23 @@
+
+package com.example.e4.rcp.todo.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.services.EMenuService;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class TodoDetailsPart {
+
+	@PostConstruct
+	public void createControls(Composite parent, EMenuService menuService) {
+		// more code...
+		TableViewer viewer = new TableViewer(parent, SWT.FULL_SELECTION | SWT.MULTI);
+
+		// more code
+
+		// register context menu on the table
+		menuService.registerContextMenu(viewer.getControl(), "com.example.e4.rcp.todo.popupmenu.table");
+	}
+}
